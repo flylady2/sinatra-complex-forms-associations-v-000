5162733213573ab33e0012edb38221c008b7ca0b
Leslie Stevens <flylady@mail.utexas.edu>
2018-11-25 20:01:25 +0000

    automatically backed up by learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -38,19 +38,11 @@ class PetsController < ApplicationController
   patch '/pets/:id' do
 
     @pet = Pet.find(params[:id])
-    #if !params[:pet].keys.include?("owner_id")
-      #@owner = Owner.find(params["owner_id"])
-    #end
-    #binding.pry
     @pet.update(params["pet"])
     if !params["owner"]["name"].empty?
       @pet.owner = Owner.create(name: params["owner"]["name"])
     end
     @pet.save
-
-  #  @owners = Owner.all
-      #@owner = Owner.create(name: params["owner_name"])
-      #@owner.pets << @pet
 
     redirect "pets/#{@pet.id}"
   end
